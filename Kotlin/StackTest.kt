964fee8c02987fce394068205e2acd9d0b649889
kdix-23-071 <2312110071r@kindai.ac.jp>
2025-07-23 00:57:00 +0900

    変更修正

--- Kotlin/StackTest.kt
+++ Kotlin/StackTest.kt
@@ -1,28 +1,35 @@
 fun isValid(s: String): Boolean {
-    val stack = mutableListOf<Char>()
-    val map = mapOf('(' to ')', '{' to '}', '[' to ']')
 
-    for (char in s) {
-        if (char in map.keys) {
-            stack.add(char)
-        } else if (char in map.values) {
-            if (stack.isEmpty() || map[stack.removeAt(stack.size - 1)] != char) {
-                return false
-            }
-        }
-    }
-    return stack.isEmpty()
+          val stack = mutableListOf<Char>()
+          //組み合わせ表
+          val map = mapOf('(' to ')', '{' to '}', '[' to ']')
+
+          //各文字チェック
+          for (char in s) {
+                    //開きかっこ検知した場合
+                    if (char in map.keys) {
+                              //スタック保存
+                              stack.add(char)
+                    //とじかっこ検知した場合          
+                    } else if (char in map.values) {
+                              //スタック空なら最初にとじかっこなのでfalse スタックに保存したキーで出たバリューが一致しないならfalse
+                              if (stack.isEmpty() || map[stack.removeAt(stack.size - 1)] != char) {
+                                        return false
+                              }
+                    }
+          }
+          return stack.isEmpty()
 }
 
 fun main() {
-    val s1 = "()"
-    val s2 = "({[]})"
-    val s3 = "({[})"
+          val s1 = "()"
+          val s2 = "({[]})"
+          val s3 = "({[})"
 
-    println("入力:s=\"$s1\"")
-    println("出力:${isValid(s1)}")
-    println("入力:s=\"$s2\"")
-    println("出力:${isValid(s2)}")
-    println("入力:s=\"$s3\"")
-    println("出力:${isValid(s3)}")
+          println("入力:s=\"$s1\"")
+          println("出力:${isValid(s1)}")
+          println("入力:s=\"$s2\"")
+          println("出力:${isValid(s2)}")
+          println("入力:s=\"$s3\"")
+          println("出力:${isValid(s3)}")
 }
